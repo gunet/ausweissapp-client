234345df02f4b51a025ac359ec3233d02befdeec
kkmanos <koukoularisman@gmail.com>
2024-06-26 18:36:42 +0300

    added simple example authentication

--- src/index.js
+++ src/index.js
@@ -1,15 +1,79 @@
 import * as ws from 'ws';
+import winston from 'winston';
+import fs from 'fs';
+import path from 'path';
 
+const sim = JSON.parse(fs.readFileSync(path.join(process.cwd(), 'simulatorCard.json'), { encoding: 'utf8'}));
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    defaultMeta: { service: 'user-service' },
+    transports: [
+      //
+      // - Write all logs with importance level of `error` or less to `error.log`
+      // - Write all logs with importance level of `info` or less to `combined.log`
+      //
+      new winston.transports.File({ filename: 'error.log', level: 'error' }),
+      new winston.transports.File({ filename: 'combined.log' }),
+    ],
+  });
 
 const sock = new ws.WebSocket("ws://ausweisapp:24727/eID-Kernel")
+let messageCounter = 0;
+
+
+const sendMessage = async (message) => {
+    sock.send(JSON.stringify(message))
+}
+
 
 sock.on('open', (ws) => {
-    sock.send(JSON.stringify({ cmd: "GET_INFO" }), (err) => {
-        console.log("Err = ", err)
-    }) 
+
+    (async function main() {
+        await sendMessage({
+            "cmd": "RUN_AUTH",
+            "tcTokenURL": "https://test.governikus-eid.de/AusweisAuskunft/WebServiceRequesterServlet",
+            "developerMode": true,
+        })
+    })()
 })
 
 
-sock.on('message', (data, isBinary) => {
-    console.log("Data = ", data.toString())
+
+
+let insertCardTry = 0; 
+let pinEntered = 0;
+
+sock.on('message', async (data, isBinary) => {
+    logger.info(`Message = ${messageCounter++}, ${data.toString()}`)
+
+    const message = JSON.parse(data.toString());
+
+    if (message.msg == 'ACCESS_RIGHTS') {
+        await sendMessage({ "cmd": "ACCEPT" });
+    }
+
+    if (message.msg == 'INSERT_CARD' && insertCardTry == 0) {
+        await sendMessage({
+            "cmd": "SET_CARD",
+            "name": "Simulator",
+            "simulator": sim
+        });
+        insertCardTry++;
+    }
+
+
+    if (message.msg == 'ENTER_PIN' && pinEntered == 0) {
+        await sendMessage({"cmd": "SET_PIN" })
+        pinEntered++;
+    }
+
+    // if (message.msg == "AUTH" && message.result) {
+    //     logger.info(`Result = ${JSON.stringify(message.result)}`)
+    // }
+
+    // if (message.msg == 'ACCESS_RIGHTS') {
+    //     await sendMessage({ cmd: "ACCEPT" })
+    // }
+
 })
